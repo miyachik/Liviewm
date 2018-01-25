604b11423d1db35bb30babed610ff1b797ddaf28
miyachik <atto.mark56@gmail.com>
2018-01-25 16:46:45 +0900

    pumaの起動タスク追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,16 @@ namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
+      before 'deploy:restart', 'puma:start'
       invoke 'deploy'
+    end
+  end
+
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
     end
   end
 
